de8e9d5992ce56d4990791df5af3ce110386ab6b
AaronCoday73 <aaron.coday@gmail.com>
2024-11-09 08:42:53 -0800

    Initial Commit

---
+++ FirstCIProject/FirstCIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstCIProjectApp.swift
+//  FirstCIProject
+//
+//  Created by Aaron Coday on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstCIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
